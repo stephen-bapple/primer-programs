2a91a17e62eff70cffa3be8de4b3018270c75356
Stephen Bapple <stephen.bapple@gmail.com>
2018-10-27 12:02:11 -0600

    Completed array traversal and typing exercises.

---
+++ chapter3/prog3e44.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iterator>
+
+using std::cout; using std::endl;
+
+int main()
+{
+    // ia is an array of 3 arrays of 4 ints
+    int ia[3][4] ={{0, 1, 2, 3},
+                   {4, 5, 6, 7},
+                   {8, 9, 10, 11}};
+    // degrades to a pointer to an array of 4 ints. Pointing at 1st array
+    // out of the three arrays.
+
+    typedef int (&int_array)[4];
+    cout << "[ ";
+    for (int_array row : ia)
+    {
+        for (int col : row)
+        {
+            cout << col << ' ';
+        }
+    }
+    cout << "]" << endl;
+
+    cout << "[ ";
+    for (int row = 0; row != 3; row++)
+    {
+        for (int col = 0; col != 4; col++)
+        {
+            cout << ia[row][col] << ' ';
+        }
+    }
+    cout << "]" << endl;
+
+    typedef int (*array_ptr)[4];
+    cout << "[ ";
+    for (array_ptr p = ia; p != ia + 3; p++) // p is a ptr to an array of 4 ints
+    {
+        for (int *q = *p; q != *p + 4; q++) // q is a pointer to int
+        {                                   // *p is an array of 4 ints
+            cout << *q << ' ';  // *q is an int
+        }
+    }
+    cout << "]" << endl;
+}
+
---
+++ chapter3/prog3e45.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iterator>
+
+using std::cout; using std::endl;
+
+int main()
+{
+    // ia is an array of 3 arrays of 4 ints
+    int ia[3][4] ={{0, 1, 2, 3},
+                   {4, 5, 6, 7},
+                   {8, 9, 10, 11}};
+    // degrades to a pointer to an array of 4 ints. Pointing at 1st array
+    // out of the three arrays.
+
+    cout << "[ ";
+    for (auto &row : ia)
+    {
+        for (int col : row)
+        {
+            cout << col << ' ';
+        }
+    }
+    cout << "]" << endl;
+
+    cout << "[ ";
+    for (auto row = 0; row != 3; row++)
+    {
+        for (auto col = 0; col != 4; col++)
+        {
+            cout << ia[row][col] << ' ';
+        }
+    }
+    cout << "]" << endl;
+
+    cout << "[ ";
+    for (auto p = ia; p != ia + 3; p++) // p is a ptr to an array of 4 ints
+    {
+        for (auto q = *p; q != *p + 4; q++) // q is a pointer to int
+        {                                   // *p is an array of 4 ints
+            cout << *q << ' ';  // *q is an int
+        }
+    }
+    cout << "]" << endl;
+}
+
